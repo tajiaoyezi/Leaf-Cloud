4ec56402239bfca0aac2b0ac0cf4daadfe4cba35
tajiaoyezi <tajiaoyezi@163.com>
2023-07-26 22:34:30 +0800

    :sparkles: Result Code Enumeration

---
+++ leaf-common/leaf-common-core/src/main/java/com/leaf/common/core/enums/ResultCode.java
@@ -0,0 +1,30 @@
+package com.leaf.common.core.enums;
+
+/**
+ * 响应状态码枚举
+ *
+ * @author yezi
+ * @date 2023/7/26 22:19
+ */
+public enum ResultCode {
+    SUCCESS(200, "操作成功"),
+    FAILED(500, "操作失败"),
+    VALIDATE_FAILED(404, "参数检验失败"),
+    UNAUTHORIZED(401, "暂未登录或token已经过期"),
+    FORBIDDEN(403, "没有相关权限");
+    private long code;
+    private String message;
+
+    ResultCode(long code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
